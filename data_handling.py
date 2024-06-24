ef304e5da4413e53e781fc0d0929213c7dfbfb69
David Nogueras <davidnogueras@MacBook-Pro-de-David.local>
2024-06-24 11:36:15 +0200

    Mejoras en la descripcion de los comentarios de data handling

--- data_handling.py
+++ data_handling.py
@@ -1,29 +1,31 @@
 import pandas as pd
 
 # Creo la funcion para cargar los datos
-# Recibo solo el path del archivo del arhico
+# Recibo solo el path del archivo del arhivo
+
+
 def read_csv(path):
     df = pd.read_csv(path)
-    print(df.head(5)) # Muetro primeras 5 lineas
-    print(df.info()) #Muestro estructura del df
+    print(df.head(5))  # Muetro primeras 5 lineas
+    print(df.info())  # Muestro estructura del df
     return df
 
+
+# Funcion para dejar las columnas solicitadas
 def clean_csv(df):
-    columns_to_keep = ['month', 'state', 'permit', 'handgun','long_gun']
+    columns_to_keep = ['month', 'state', 'permit', 'handgun', 'long_gun']
     df_clean = df.loc[:, columns_to_keep]
     print(df_clean.info())  # Muestro estructura del df
-    return (df_clean)
+    return df_clean
 
+
+# Renombrar columna de long_gun
 def rename_col(df):
     column = "long_gun"
     new_column = "longgun"
     if column in df.columns:
-        df_renamed = df.rename(columns = {column:new_column})
-        return (df_renamed)
+        df_renamed = df.rename(columns={column: new_column})
+        return df_renamed
     else:
         print("La columna {} ya existe".format(new_column))
-        return (df)
-
-
-
-
+        return df
